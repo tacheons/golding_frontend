0b3cd5baf8e61cf10f08c457fba2448ceba5944d
Fabian Madueke <fabtronss@gmail.com>
2024-11-29 17:19:28 +0100

    Add singup.js

---
+++ src/modules/signup.js
@@ -0,0 +1,39 @@
+
+/**
+ * @implementaionNote
+ * Company Controller, is the controller layer that controls
+ * various services offered by the company
+ *
+ * @return
+ * an array of services
+ *
+ * @author
+ * @since
+ * v1.0.0
+ */
+export const signup = async (email, password) => {
+    setIsLoading(true)
+    setError(null)
+
+    const response = await fetch('/api/user/signup', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ email, password })
+    })
+    const json = await response.json()
+
+    if (!response.ok) {
+      setIsLoading(false)
+      setError(json.error)
+    }
+    if (response.ok) {
+      // save the user to local storage
+      localStorage.setItem('user', JSON.stringify(json))
+
+      // update the auth context
+      dispatch({type: 'LOGIN', payload: json})
+
+      // update loading state
+      setIsLoading(false)
+    }
+  }
