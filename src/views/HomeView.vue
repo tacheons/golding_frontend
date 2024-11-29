1b1da189500c111ca730208dc8a7ca30be6f0c85
Fabian Madueke <fabtronss@gmail.com>
2024-11-29 17:20:07 +0100

    Add Home View

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,43 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Header from "../components/Header.vue";
 </script>
 
+<!--
+- Home page, this is the landing page of the application
+- with no restricted functions and menues
+-->
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="row">
+    <Header />
+
+    <div class="row">
+      <div class="home">
+        <h1>This is the Home page</h1>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+.home {
+  width: 100%;
+  min-height: 100vh;
+  /* border: ridge; */
+}
+
+.headrow {
+  border-bottom: groove 3px;
+  border-color: #9e9e9e;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+.menu {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* border: ridge 3px; */
+}
+</style>
