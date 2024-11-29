ad94cc68bdf26f4e474e7bef12fa51331024182e
Fabian Madueke <fabtronss@gmail.com>
2024-11-29 17:20:39 +0100

    Add index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import SignupView from "../views/SignupView.vue";
+import DashboardView from "../views/DashboardView.vue";
 
+/**
+ * @implementation
+ * Route file for the configuration various routes
+ *
+ * @authors
+ * @since
+ * v1.0.0
+ */
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "Home",
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/dashboard",
+      name: "Dashboard",
+      component: DashboardView,
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: LoginView,
+    },
+    {
+      path: "/signup",
+      name: "Signup",
+      component: SignupView,
     },
   ],
-})
+});
 
-export default router
+export default router;
